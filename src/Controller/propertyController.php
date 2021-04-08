be30fb1d35dea36a6c48802e6c25ab5b18fc398a
Remi_Cahuzac <cahuzac.remi@gmail.com>
2021-04-08 13:26:59 +0200

    Ajout de filtre.

--- src/Controller/propertyController.php
+++ src/Controller/propertyController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -38,17 +40,25 @@ class propertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
     public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
+
         $properties = $paginator->paginate(
-            $this->repository->findAllVisible(),
+            $this->repository->findAllVisibleQuery($search),
             $request->query->getInt('page',1), 12
         );
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
-            'properties' => $properties
+            'properties' => $properties,
+            'form' => $form->createView()
         ]);
     }
 
